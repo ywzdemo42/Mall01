ae0f4b81c9374304d8db2561f5917502d6cdc3ce
ywzdemo42 <408083094@qq.com>
2019-11-15 01:07:53 +0800

    增加导航栏和axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import router from "./router";
 import App from './App.vue'
-import ElementUI from 'element-ui'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,22 +1,46 @@
 <template>
   <div id="home">
-    <nav-bar backcolor="pink">
-      <div slot="center" >购物街</div>
+    <nav-bar backcolor="pink" >
+      <div slot="center" style="color: #f6f6f6">购物街</div>
     </nav-bar>
-    <h2>Home</h2>
+    <span class="demonstration">默认 Hover 指示器触发</span>
+    <el-carousel height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+    <div class="block">
+      <span class="demonstration">Click 指示器触发</span>
+      <el-carousel trigger="click" height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  import {getHomeMultidata} from "network/home";
+
   export default {
     name: "Category",
     components: {
       NavBar
+    },
+    created() {
+      getHomeMultidata()
+              .then(res => {
+                console.log(res)
+              })
+              .catch()
     }
   }
 </script>
 
 <style scoped>
-
+  #home {
+    background-color: darkgrey;
+  }
 </style>
